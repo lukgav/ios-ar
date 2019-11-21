9a9e53e5f36e68df42eea275e4c0945a2621e643
crabman84 <lukegavin.5@outlook.com>
2019-11-21 18:02:01 +0100

    test commit

---
+++ tutorials/luke/Plane Detection/Plane Detection/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Plane Detection
+//
+//  Created by Luke Gavin on 21.11.19.
+//  Copyright © 2019 Luke Gavin. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+
+
+class ViewController: UIViewController {
+
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
