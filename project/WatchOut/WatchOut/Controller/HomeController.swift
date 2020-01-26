84e9998781ea58dcd725eb2b32962eba748b9237
crabman84 <lukegavin.5@outlook.com>
2020-01-26 23:55:04 +0100

    + TODO: finish doubleTapLight code in homecontroller

--- project/WatchOut/WatchOut/Controller/HomeController.swift
+++ project/WatchOut/WatchOut/Controller/HomeController.swift
@@ -18,12 +18,22 @@ class HomeController {
     //AmbientLight sensor variables
     private let lsManager = LightSensorManager.shared
     private let ambientIntensityObserver = AmbientIntensityObserver()
-    
     private var ignoreLightCounter: Int = 0
     private let ignoreLightCount: Int = 2
     private var isAmbientIntensityDark: Bool = false
+    private var currentLightIntensity: Double?
 
+    //Timer variables
     private var timer: Timer
+    // Time is in seconds
+    private let timeInterval: Double = 1
+    private var waitTime: Double?
+    private var coverLightOnce = false
+    private var tappedLight = false
+    private var coverLightTwice = false
+    private var doubleTappedLight = false
+    private var _maxIntensity: Double?
+    private var _minIntensity: Double?
     
     var playerCount: Int = 2
     
@@ -32,6 +42,60 @@ class HomeController {
     init(homeViewController: HomeViewController) {
         self.homeViewController = homeViewController
         timer = Timer()
+        _maxIntensity = 1000
+        _minIntensity = 750
+    }
+    
+    func checkifTapped(totalDuration: Double){
+        waitTime = totalDuration
+        timer = Timer.scheduledTimer(withTimeInterval:self.timeInterval, repeats: true){timer in
+            
+            self.waitTime! -= self.timeInterval
+//            print("IN TIMER")
+            print("---------------------------------------------")
+            print("Was Light: \(!self.isAmbientIntensityDark)")
+            print("Is Dark: \(self.isLightCurrentlyOff())")
+            print("Tapped Light: \(self.tappedLight)")
+            print ("IS True: \(!self.isAmbientIntensityDark && self.isLightCurrentlyOff() && self.tappedLight)")
+            if (self.doubleTappedLight){
+                self.playerCount += 1
+            }
+
+            if (self.waitTime! < 0.0) {
+                print("--------------------------------Timer is FINISHED--------------------------------")
+                timer.invalidate()
+                // Timer ran out
+            }
+            if (self.isAmbientIntensityDark && self.isLightCurrentlyOn() && self.coverLightTwice) {
+                    self.isAmbientIntensityDark = false
+                    print("Light sensor has been TAPPED TWICE")
+            }
+            if (self.isAmbientIntensityDark && self.isLightCurrentlyOn()) {
+                    self.isAmbientIntensityDark = false
+                    self.tappedLight = true
+                    print("Light sensor has been TAPPED ONCE")
+            }
+            if(!self.isAmbientIntensityDark && self.isLightCurrentlyOff() && self.tappedLight){
+                print("!!!!!!!!!!!!!Light sensor COVERED TWICE!!!!!!!!!!!!!!")
+
+                self.isAmbientIntensityDark = true
+                self.coverLightTwice = true
+//                print("Light sensor COVERED TWICE")
+            }
+                
+                //print("CountDownTime: \(self.countDownTime)")
+//                self.deliverViewController.updateTimerLabel(newTime: self.countDownTime)
+            
+            
+            return
+        }
+    }
+    
+    func isLightCurrentlyOn()->Bool{
+        return self.currentLightIntensity! > _maxIntensity!
+    }
+    func isLightCurrentlyOff()->Bool{
+        return self.currentLightIntensity! < self._minIntensity!
     }
     
     func ambientLightOn(maxIntensity: Double, minIntensity: Double){
@@ -39,21 +103,24 @@ class HomeController {
         if(isLightSensorOn) {
             print("-----------homescreen: LIGHT sensor ON----------")
             lsManager.ambientIntensity.addObserver(ambientIntensityObserver) { newIntensity in
+                self.currentLightIntensity = newIntensity
                 // Ignore first values, because of light change on beginning
                 if (self.ignoreLightCounter > self.ignoreLightCount) {
-                    print("Intensity: \(newIntensity)")
-                    if (self.isAmbientIntensityDark == false && newIntensity < minIntensity) {
+//                    print("Intensity: \(newIntensity)")
+                    if (self.isAmbientIntensityDark == false && newIntensity < minIntensity && !self.coverLightOnce) {
                         self.isAmbientIntensityDark = true
+                        self.coverLightOnce = true
                         // user has thumb on light sensor
+                        self.checkifTapped(totalDuration: 10)
                         print("THUMB NOW ON SENSOR")
                    }
-                   else if (self.isAmbientIntensityDark == true && newIntensity > maxIntensity) {
-                       self.isAmbientIntensityDark = false
-                       // user lifted thumb and delivered to next player
-                        print("THUMB LEFT THE SENSOR")
-                        isLightSensorOn = self.lsManager.stopLightSensor()
-                        self.navigateToFirstTask()
-                   }
+//                   else if (self.isAmbientIntensityDark == true && newIntensity > maxIntensity) {
+//                       self.isAmbientIntensityDark = false
+//                       // user lifted thumb and delivered to next player
+//                        print("THUMB LEFT THE SENSOR")
+//                        isLightSensorOn = self.lsManager.stopLightSensor()
+//                        self.navigateToFirstTask()
+//                   }
                 }
                 else {
                         print("Ignored Intensity: \(newIntensity)")
@@ -64,7 +131,7 @@ class HomeController {
     }
     
     func startGame(){
-        self.ambientLightOn(maxIntensity: 1000, minIntensity: 750)
+        self.ambientLightOn(maxIntensity: _maxIntensity!, minIntensity: _minIntensity!)
 //        {
 //            navigateToFirstTask()
 //        }
