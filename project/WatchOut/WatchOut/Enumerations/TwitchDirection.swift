748802fb1edecc7ca588b127674792b8d930c1aa
andreasstiller <33620548+andreasstiller@users.noreply.github.com>
2020-01-31 01:22:25 +0100

    + added comments

--- project/WatchOut/WatchOut/Enumerations/TwitchDirection.swift
+++ project/WatchOut/WatchOut/Enumerations/TwitchDirection.swift
@@ -5,6 +5,9 @@
 //  Created by iOS1920 on 23.01.20.
 //  Copyright © 2020 iOS1920. All rights reserved.
 //
+
+// direction in which the user can twitch
+// if more direction should be supported, add them here
 enum TwitchDirection {
     case Up
     case Right
