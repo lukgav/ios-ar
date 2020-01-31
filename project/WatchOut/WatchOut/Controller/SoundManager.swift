767024eafac2efc8f13e55be56cb97a2f2bcf264
andreasstiller <33620548+andreasstiller@users.noreply.github.com>
2020-01-31 01:58:57 +0100

    + added more comments

--- project/WatchOut/WatchOut/Controller/SoundManager.swift
+++ project/WatchOut/WatchOut/Controller/SoundManager.swift
@@ -11,12 +11,18 @@ import AVFoundation
 
 class SoundManager {
     
+    // singleton
     static let shared = SoundManager()
     
+    // must keep reference to audioplayer
+    // other the sound will stop playing after the method where it is used is at the end
     private var tickSound: AVAudioPlayer?
     private var bombSound: AVAudioPlayer?
+    
     private var timer: Timer = Timer()
     
+    // maximum and minimum time intervals for sounds
+    // can be changed without problems
     var maxUpdateInterval: Double = 2.5
     var minUpdateInterval: Double = 1/10.0
     
@@ -33,11 +39,13 @@ class SoundManager {
         // initialize with above the boundaries
         lastPositionInBoundaries = -1
         
+        // urls for loading the soudns
         let tickPath = Bundle.main.path(forResource: "TickSound.mp3", ofType:nil)!
         let tickUrl = URL(fileURLWithPath: tickPath)
         let bombPath = Bundle.main.path(forResource: "BombSound.wav", ofType:nil)!
         let bombUrl = URL(fileURLWithPath: bombPath)
         
+        // load images inside do-catch
         do {
             tickSound = try AVAudioPlayer(contentsOf: tickUrl)
             tickSound?.volume = 1.0
@@ -50,6 +58,8 @@ class SoundManager {
         }
     }
     
+    // only start a new ticking timer, when the new update interval is in another boundary/interval
+    // (explained at the top)
     func playTickSound(newUpdateInterval: Double) {
         var updateInterval: Double
         var newPositionInBoundaries : Int
@@ -79,7 +89,6 @@ class SoundManager {
             timer.invalidate()
             timer = Timer(fire: Date(), interval: updateInterval, repeats: true,
                                block: { (timer) in
-//                                print("Playing Tick Sound with Interval: \(updateInterval)")
                                 self.tickSound?.play()
             })
 
