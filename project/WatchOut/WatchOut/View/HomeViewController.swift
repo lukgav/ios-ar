7ff6d12706c66f1a38f8822da09f26d349c598c6
andreasstiller <33620548+andreasstiller@users.noreply.github.com>
2020-01-31 01:46:47 +0100

    + added comments

--- project/WatchOut/WatchOut/View/HomeViewController.swift
+++ project/WatchOut/WatchOut/View/HomeViewController.swift
@@ -17,7 +17,6 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
         controller = HomeController(homeViewController: self)
         controller?.startGame()
     }
