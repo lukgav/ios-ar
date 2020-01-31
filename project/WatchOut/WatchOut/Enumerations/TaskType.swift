d5a27fc826ceb409f818f25d19eb0c5276c5c1b9
andreasstiller <33620548+andreasstiller@users.noreply.github.com>
2020-01-31 01:21:36 +0100

    + added comments

--- project/WatchOut/WatchOut/Enumerations/TaskType.swift
+++ project/WatchOut/WatchOut/Enumerations/TaskType.swift
@@ -5,6 +5,8 @@
 //  Created by Guest User on 16/01/2020.
 //  Copyright © 2020 iOS1920. All rights reserved.
 //
+
+// if more tasks are added, add them here too
 enum TaskType {
     case Unwrap
     case Deliver
